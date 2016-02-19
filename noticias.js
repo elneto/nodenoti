cd6adb5d6e995ba3acd5d7a745c49c7823d9a0c3
Ernesto Araiza <yo@ernestoaraiza.com>
2016-02-19 01:57:16 -0500

    exiting correct when there are no news

--- noticias.js
+++ noticias.js
@@ -1,23 +1,24 @@
 var scraperjs = require("scraperjs");
 var mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/noti');
+mongoose.connect('mongodb://localhost:27017/noti',  {server:{auto_reconnect:true}});
 
 //Entrada is a model
 var Entrada = mongoose.model('Entrada', require('./entrada'));
 //var urlCrawl = 'http://www.un.org/spanish/News/latest-headlines.asp';
 var urlCrawl = 'http://localhost:8008/test.html';
 
+var entriesToSave = 0;
+var foundNews = 0;
+var oldNews = 0;
+
 scraperjs.StaticScraper.create(urlCrawl)
     .scrape(function($) {
         return $("p a").map(function() {
-        	//console.log($(this));
-            //return $(this).text();
             return $(this);
         }).get();
     })
     .then(function(news) {
-        //console.log(news);
         listArray(news); 
     });
 
@@ -29,11 +30,23 @@ function codigo(url){
 	return url.split("=")[1];
 }
 
+function handleError(err){
+	console.error("Error: " + err); 
+	return 1; //error
+}
+
+function tryCloseMongo(){
+	if (--entriesToSave <= 0){
+  	console.log("disconnect from mongo, entriesToSave: " + entriesToSave);
+		mongoose.disconnect();
+		return 0;
+  }
+}
+
 function listArray(news){
+	foundNews = news.length-1; //el de mas noticias
+	console.log("Found news: " + foundNews);
 	news.forEach(function(val){
-		/*console.log("title: " + val.text());
-		console.log("link: " + completa(val.attr('href')));
-		console.log("code: " + codigo(val.attr('href')));*/
 
 		var ent = new Entrada({
 			title: val.text(),
@@ -41,7 +54,29 @@ function listArray(news){
 			code: codigo(val.attr('href'))
 		});
 
-		console.log(ent);
+		//let's see if the code is saved already
+		var query  = Entrada.where({ code: ent.code });
+		query.findOne(function (err, entry) {
+		  if (err) return handleError(err);
+		  if (!entry && ent.code !== undefined) { //if not found
+		    // doc may be null if no document matched
+		    console.log("Let's save " + (++entriesToSave) + " " + ent.title + " " + ent.code);
+		    ent.save(function (err) {
+				  if (err) return handleError(err);
+				  tryCloseMongo();
+				});
+		  } else if (entry){
+		  	console.log("Old news: " + (++oldNews));
+		  	if (oldNews == foundNews){ //nothing new
+		  		tryCloseMongo();
+		  	}
+		  }
+		});
+
+		
 	});
+
+	//tryCloseMongo();
+	return 0;
 }
 
