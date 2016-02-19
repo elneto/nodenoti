af3ea30ae4a3f0ac763bc04f01ff93e6415434e4
Ernesto Araiza <yo@ernestoaraiza.com>
2016-02-18 22:54:30 -0500

    separa el titulo, link y codigo

--- noticias.js
+++ noticias.js
@@ -1,19 +1,36 @@
-var scraperjs = require("scraperjs")
+var scraperjs = require("scraperjs");
+var mongoose = require("mongoose");
+
+//mongoose.connect('mongodb://localhost:27017/noti');
+
+//Entrada is a model
+//var Entrada = mongoose.model('Entrada', require('./entrada'));
 
 scraperjs.StaticScraper.create('http://www.un.org/spanish/News/latest-headlines.asp')
     .scrape(function($) {
         return $("p a").map(function() {
         	//console.log($(this));
-            return $(this).text();
+            //return $(this).text();
+            return $(this);
         }).get();
     })
     .then(function(news) {
         //console.log(news);
         listArray(news);
-    })
+    });
+
+function completa(url){
+	return 'http://www.un.org' + url;
+}
+
+function codigo(url){
+	return url.split("=")[1];
+}
 
 function listArray(news){
 	news.forEach(function(val, index){
-		console.log(index + " is: " + val);
+		console.log("title: " + val.text());
+		console.log("href: " + completa(val.attr('href')));
+		console.log("codigo: " + codigo(val.attr('href')));
 	});
 }
