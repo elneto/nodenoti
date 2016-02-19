22324094c334156ccb2bc127319fcc58a6cbbb71
Ernesto Araiza <yo@ernestoaraiza.com>
2016-02-18 21:54:34 -0500

    accessing each array element in a function

--- noticias.js
+++ noticias.js
@@ -3,10 +3,17 @@ var scraperjs = require("scraperjs")
 scraperjs.StaticScraper.create('http://www.un.org/spanish/News/latest-headlines.asp')
     .scrape(function($) {
         return $("p a").map(function() {
-        	console.log($(this));
+        	//console.log($(this));
             return $(this).text();
         }).get();
     })
     .then(function(news) {
-        console.log(news);
+        //console.log(news);
+        listArray(news);
     })
+
+function listArray(news){
+	news.forEach(function(val, index){
+		console.log(index + " is: " + val);
+	});
+}
